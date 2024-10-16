fd48cbf176eeda7d766ae6be5f60b36342d2538c
nachikett01 <nachikettekade01@gmail.com>
2024-10-16 22:28:52 +0530

    major

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default{
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // include all JS, JSX, TS, and TSX files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
