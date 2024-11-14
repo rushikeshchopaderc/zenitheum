907faa7480c7f4856a354d781d6b51aa4ed9a117
rushikeshchopaderc <rushikeshchopaderc@gmail.com>
2024-11-14 11:57:43 +0530

    navfix_12

--- src/components/Navbar/NavTab.jsx
+++ src/components/Navbar/NavTab.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NavTab = ({ label, isActive }) => {
-  const baseClasses = "flex items-center justify-center px-4 py-2 md:py-6 border-b md:border-none";
+  const baseClasses = "flex items-center text-xl justify-center px-4 py-2 md:py-6 border-b md:border-none";
   const activeClasses = "text-blue-700 bg-white";
   const inactiveClasses = "bg-zinc-900 text-white";
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,40 +32,40 @@ const Navbar = () => {
         currentPage === "/" ? "bg-white" : "bg-zinc-900"
       } border-b border-zinc-700 max-w-full`}
     >
-      <div className="flex justify-between items-center h-16 px-4 bg-zinc-900 text-white">
-        <Link to="/" className="flex items-center">
-          <img src={Logo} alt="Company logo" className="w-12 h-auto" />
-        </Link>
-
+      <div className="flex justify-between items-center w-[100%] bg-zinc-900 text-white md:flex md:justify-around">
+        <div className="hidden md:flex md:items-center md:justify-around">
+          {navItems.slice(0, 4).map((item, index) => (
+            <Link
+              key={index}
+              to={item.to}
+              className="hover:text-blue-700 transition duration-300 ease-in-out block md:inline-block"
+            >
+              <NavTab label={item.label} isActive={currentPage === item.to} />
+            </Link>
+          ))}
+        </div>
+        <div className="md:flex md:items-center md:justify-center">
+          <Link to="/" className="flex items-center justify-center">
+            <img src={Logo} alt="Company logo" className="w-12 justify-around items-center h-auto" />
+          </Link>
+        </div>
+        <div className="hidden md:flex md:items-center md-justify-around">
+          {navItems.slice(5).map((item, index) => (
+            <Link
+              key={index}
+              to={item.to}
+              className="hover:text-blue-700 transition duration-300 ease-in-out block md:inline-block"
+            >
+              <NavTab label={item.label} isActive={currentPage === item.to} />
+            </Link>
+          ))}
+        </div>
         <button
           onClick={() => setMenuOpen(!menuOpen)}
           className="text-white md:hidden focus:outline-none"
         >
           {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
         </button>
-
-        <nav
-          className={`${
-            menuOpen ? "block" : "hidden"
-          } md:flex md:items-center w-full md:w-auto md:space-x-6`}
-        >
-          {navItems.map((item, index) => {
-            const isActive = currentPage === item.to;
-            const navTabClass = isActive ? 'bg-white' : '';
-
-            // if (item.type === "logo") return null; // Skip logo in nav items
-
-            return (
-              <Link
-                key={index}
-                to={item.to}
-                className="hover:text-blue-700 transition duration-300 ease-in-out block md:inline-block"
-              >
-                <NavTab label={item.label} isActive={isActive} className={navTabClass} />
-              </Link>
-            );
-          })}
-        </nav>
       </div>
     </header>
   );
